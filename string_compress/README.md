cf8b3c739609228cc4cf944c4837c159bbf7c758
Samuel Chow <cybersam201004@gmail.com>
2019-08-15 11:02:31 -0700

    Add string compress

---
+++ string_compress/README.md
@@ -0,0 +1,3 @@
+# Simple String Compression
+
+Compress a string by grouping all repeated substrings into a character followed by a count. For example aaaabcc will be compressed to a4b1c2. And if the compressed string is longer than the original string, just return the original string eg. aabc should return aabc not a2b1c1. 
---
+++ string_compress/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func compress(str string) string {
+	runes := []rune(str)
+	var sb strings.Builder
+	count := 1
+
+	for i, ch := range runes {
+		if i < len(runes) - 1 {
+			// Look 1 character ahead (but not the last one)
+			c := runes[i+1]
+			if c != ch {
+				// Start of repeated char, reset count and append current char ch
+				sb.WriteRune(ch)
+				sb.WriteString(fmt.Sprintf("%d", count))
+				count = 1
+			} else {
+				// Don't append repeated character
+				count++
+			}
+		} else {
+			// At last character
+			sb.WriteRune(ch)
+			sb.WriteString(fmt.Sprintf("%d", count))
+		}
+	}
+
+	if sb.Len() >= len(str) {
+		return str
+	}
+
+	return sb.String()
+}
+
+func main() {
+	strings := []string{
+		"a",
+		"aa",
+		"aaaa",
+		"aaaabbbccd",
+		"abcdddddd",
+		"abcc",
+	}
+
+	for _, s := range strings {
+		fmt.Println(compress(s))
+	}
+}
